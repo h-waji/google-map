18d2796f42e1fd3e43750c30af67d53d28d8bcfa
wajima <goo.hiro.w@gmail.com>
2023-04-28 15:30:58 +0900

    axios, geocode

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,32 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')! as HTMLFormElement;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'xxxxx';
+
+type GoogleGeocodingResponse = {
+  results: {geometory: {location: {lat: number, lng: number}}}[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
   console.log(enteredAddress);
+
+  axios.get<GoogleGeocodingResponse>(`https://maps.google.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+    .then(response => {
+      if (response.data.status !== 'OK') {
+        throw new Error('座標を取得できませんでした。');
+      }
+      const coordinates = response.data.results[0].geometory.location;
+      console.log(coordinates);
+    })
+    .catch(err => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener('submit', searchAddressHandler);
